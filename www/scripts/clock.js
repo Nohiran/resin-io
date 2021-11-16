0b6afd1e86ac118dbad3aeb2cdd1398177427d1b
Nohiran <nh218i@icloud.com>
2021-11-17 00:14:41 +0900

    背景画像を削除し円環を追加

--- www/scripts/clock.js
+++ www/scripts/clock.js
@@ -3,39 +3,36 @@ export class Clock {
         this.frame = document.getElementById("analog_clock_frame");
         this.hour = document.getElementById("analog_clock_hour");
         this.min = document.getElementById("analog_clock_min");
-        // this.diff = document.getElementById("time_difference");
-        // let style = window.getComputedStyle(this.frame);
-        // this.width = parseFloat(style.width);
-        // this.height = parseFloat(style.height);
-        // this.diff.width = parseFloat(this.width);
-        // this.diff.height = parseFloat(this.height);
+        this.diff0 = document.getElementById("analog_clock_diff0");
+        this.diff1 = document.getElementById("analog_clock_diff1");
         this.digital_clock = document.getElementById("digital_clock");
     }
 
     draw(date) {
         this.analog_draw(date);
-        this.time_difference_draw(date);
         this.digital_draw(date);
     }
 
     analog_draw(date) {
-        let hour = date.getHours();
-        let min = date.getMinutes();
-        let hour_deg = hour / 12.0 * 360.0 - 90.0 + min / 60.0 * 360 / 12;
-        let min_deg = min / 60.0 * 360 - 90;
-        this.hour.style.transform = "rotate(" + String(hour_deg) + "deg)";
-        this.min.style.transform = "rotate(" + String(min_deg) + "deg)";
-    }
+        let now_date = new Date();
+        let now_hour_deg = now_date.getHours() * 30.0 + now_date.getMinutes() * 0.5;
 
-    time_difference_draw(date) {
-        // if (this.diff.getContext) {
-        //     var ctx = this.diff.getContext("2d");
-        //     let radius = this.width / 2.0;
-        //     ctx.beginPath();
-        //     ctx.arc(this.width / 2.0, this.height / 2.0, radius, 0, Math.PI);
-        //     ctx.stroke();
-        //     ctx.closePath();
-        // }
+        let min = date.getMinutes();
+        let hour = date.getHours();
+        let min_deg = min * 6.0;
+        let hour_deg = hour * 30.0 + min_deg / 12.0;
+        let diff_deg = (hour_deg - now_hour_deg) % 360.0;
+        this.diff0.style.backgroundImage =
+            "conic-gradient("
+            + "from " + String(now_hour_deg) + "deg,"
+            + "black 0deg,"
+            + "white " + String(diff_deg) + "deg,"
+            + "black " + String(diff_deg) + "deg,"
+            + "black 360deg)";
+        console.log(this.diff0.style.backgroundImage);
+
+        this.hour.style.transform = "rotate(" + String(hour_deg - 90) + "deg)";
+        this.min.style.transform = "rotate(" + String(min_deg - 90) + "deg)";
     }
 
     digital_draw(date) {
